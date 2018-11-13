bf862be8bf7f06e55683c5d5b91af2b5c8688c75
Rumpelszkin <kolbuszewskimax@gmail.com>
2018-11-13 03:28:06 +0100

    UJE

---
+++ app/src/main/java/com/varvet/barcodereadersample/Main2Activity.java
@@ -0,0 +1,66 @@
+package com.varvet.barcodereadersample;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.media.Image;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+
+public class Main2Activity extends AppCompatActivity {
+
+
+    public final static int WHITE = 0xFFFFFFFF;
+    public final static int BLACK = 0xFF000000;
+    public final static int WIDTH = 400;
+    public final static int HEIGHT = 400;
+    public final static String STR = "HEHEHEHE DUPA";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        Intent i = getIntent();
+        String text = i.getStringExtra ("TextBox");
+
+        ImageView imageView = (ImageView) findViewById(R.id.myImage);
+        try{
+            Bitmap bitmap = encodeAsBitmap(text);
+            imageView.setImageBitmap(bitmap);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    Bitmap encodeAsBitmap(String str) throws WriterException {
+        BitMatrix result;
+        try {
+            result = new MultiFormatWriter().encode(str,
+                    BarcodeFormat.QR_CODE, WIDTH, HEIGHT, null);
+        } catch (IllegalArgumentException iae) {
+            // Unsupported format
+            return null;
+        }
+
+        int w = result.getWidth();
+        int h = result.getHeight();
+        int[] pixels = new int[w * h];
+        for (int y = 0; y < h; y++) {
+            int offset = y * w;
+            for (int x = 0; x < w; x++) {
+                pixels[offset + x] = result.get(x, y) ? BLACK : WHITE;
+            }
+        }
+
+        Bitmap bitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+        bitmap.setPixels(pixels, 0, w, 0, 0, w, h);
+        return bitmap;
+    }
+}
+
--- app/src/main/java/com/varvet/barcodereadersample/MainActivity.kt
+++ app/src/main/java/com/varvet/barcodereadersample/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import com.google.android.gms.common.api.CommonStatusCodes
 import com.google.android.gms.vision.barcode.Barcode
@@ -13,15 +14,22 @@ import com.varvet.barcodereadersample.barcode.BarcodeCaptureActivity
 class MainActivity : AppCompatActivity() {
 
     private lateinit var mResultTextView: TextView
+    private lateinit var editText: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         mResultTextView = findViewById(R.id.result_textview)
+        editText = findViewById(R.id.TextBox);
 
         findViewById<Button>(R.id.scan_barcode_button).setOnClickListener {
             val intent = Intent(applicationContext, BarcodeCaptureActivity::class.java)
+            startActivityForResult(intent, BARCODE_READER_REQUEST_CODE)
+        }
+        findViewById<Button>(R.id.generate_qr).setOnClickListener {
+            val intent = Intent(applicationContext, Main2Activity::class.java)
+            intent.putExtra("TextBox",editText.getText().toString());
             startActivityForResult(intent, BARCODE_READER_REQUEST_CODE)
         }
     }
